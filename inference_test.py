adaba57bc582ec8ef20eef63e6edfe59a8ed482d
Kerem Yıldız <yildizkerem8085@gmail.com>
2025-04-26 21:33:51 +0300

    Update inference_test.py

--- inference_test.py
+++ inference_test.py
@@ -1,8 +1,8 @@
 from inference_sdk import InferenceHTTPClient
-your_image = "test\istock.jpg"  # Replace with your image path
+your_image = r"test\istock.jpg"  # Replace with your image path
 CLIENT = InferenceHTTPClient(
     api_url="https://serverless.roboflow.com",
-    api_key="API_KEY"
+    api_key="rf_Jnfc9ahHilQtL7iLHYu2ObiyOoB2"
 )
 
 result = CLIENT.infer(your_image, model_id="helmet-atofc/2")
